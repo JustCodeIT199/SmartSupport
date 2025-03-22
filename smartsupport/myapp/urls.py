20941cf4f552d01e091a7ff6cc79fdbd9eb2aa52
JustCodeIT199 <justcodeit199@gmail.com>
2025-03-23 02:34:57 +0530

    Improved login message handling and UI in base template

--- smartsupport/myapp/urls.py
+++ smartsupport/myapp/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from myapp import views 
+
 urlpatterns = [
    path('base/',views.Basepage,name="base"),
-   path('login/',views.LoginView ,name="login"),
+   path('login/',views.LoginView,name="login"),
+
+
+ 
+   
+  
+
    
 ]
--- smartsupport/myapp/views.py
+++ smartsupport/myapp/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from myapp.forms import LoginForm
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 
 # Create your views here.
 def Basepage(request):
     return render(request,'Base.html')
 
+#Login View
 def LoginView(request):
-    return render(request,'Login.html')
+    if request.method =='POST':
+        forms =LoginForm(request.POST)
+        if forms.is_valid():
+            username=forms.cleaned_data['username']
+            password=forms.cleaned_data['password']
+            user=authenticate(request,username=username, password=password)
+            if user is not None:
+                login(request,user)
+                messages.success(request,"✅ Login Successful! Welcome back!")
+                return render(request,'Base.html')
+            else:
+                messages.error(request,"Username or Password not Valid")
+                return render(request,'Login.html',{'forms':forms})
+    else:
+        forms =LoginForm()
+        return render(request,'Login.html',{'forms':forms})
     

53657b102e37eca541aa60d5b05ef8ef07474a59
JustCodeIT199 <justcodeit199@gmail.com>
2025-03-26 21:49:05 +0530

    Login Page Update with css ;)

--- smartsupport/myapp/urls.py
+++ smartsupport/myapp/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
    path('login/',views.LoginView,name="login"),
    path('logout/',views.LogoutView,name="logout"),
    path('register/',views.RegisterView,name="register"),
-   path('admin/', admin.site.urls),
    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'),name='password_reset'),
    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name='password_reset_done'),
    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),

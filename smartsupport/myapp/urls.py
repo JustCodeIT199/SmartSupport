b6afb21dcfa1ec939b40625d5f9ecfc9cc099ca4
JustCodeIT199 <justcodeit199@gmail.com>
2025-03-26 17:06:28 +0530

    Forget Passowrd

--- smartsupport/myapp/urls.py
+++ smartsupport/myapp/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from myapp import views 
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
    path('base/',views.Basepage,name="base"),
    path('login/',views.LoginView,name="login"),
    path('logout/',views.LogoutView,name="logout"),
    path('register/',views.RegisterView,name="register"),
+   path('admin/', admin.site.urls),
+   path('password-reset/',auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'),name='password_reset'),
+   path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name='password_reset_done'),
+   path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),
+   path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),
+ 
+
 
 
  
--- smartsupport/smartsupport/settings.py
+++ smartsupport/smartsupport/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'justcodeit199@gmail.com'
+EMAIL_HOST_PASSWORD = 'jjwg plpd fjke pflv'
+
+
